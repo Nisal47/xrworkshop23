a3bf1e6a2152e6e14ae3df74d136ec86b028f064
Hansi <hansiudapola2@gmail.com>
2023-02-10 08:37:51 +1300

    Startup Scene added

---
+++ Assets/Scripts/LoadNext.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNext : MonoBehaviour
+{
+    public void LoadMe(string sceneName){
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+}
